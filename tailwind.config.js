0a4affb6e34453740cf054c437a35e5dee9108b5
Marijn Wijbenga(home) <m.wijbenga@outlook.com>
2021-09-10 09:50:01 +0200

    completed branding component, added routing and changed folder structure
    completed branding component, added routing and changed folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   prefix: '',
   purge: {
-    enabled: process.env.NODE_ENV === 'production',
+    // enabled: process.env.NODE_ENV === 'production',
     content: [
       './src/**/*.{html,ts}',
     ]
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['"Nunito"', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
