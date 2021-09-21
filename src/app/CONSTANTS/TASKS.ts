cdb344564db803e04669aec6839e9aa779aaad5b
Marijn Wijbenga(home) <m.wijbenga@outlook.com>
2021-09-21 13:11:24 +0200

    created basic task add and view components

---
+++ src/app/CONSTANTS/TASKS.ts
@@ -0,0 +1,29 @@
+import { TaskModel } from '../models/task.model';
+
+export const TASKS: TaskModel[] = [
+  {
+    title: 'Do groceries',
+    important: false
+  },
+  {
+    title: 'Walk dog',
+    important: true
+  },
+  {
+    title: 'Play some Apex',
+    important: false
+  },
+  {
+    title: 'Play a lot of Minecraft',
+    important: true
+  },
+  {
+    title: 'Go to the forrest with kiek',
+    important: false
+  },
+  {
+    title: 'Go to the beach with Kiek',
+    important: false
+  }
+
+];
---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+import { TaskModel } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: 'add-task.component.html',
+  styleUrls: ['add-task.component.scss']
+})
+export class AddTaskComponent {
+
+  constructor(private taskService: TaskService) {
+
+  }
+
+  showSubmitButton: boolean = false;
+  showTip: boolean = false;
+
+  @Input() placeholder = '';
+
+  taskTitle: string = '';
+  important: boolean = false;
+  task: TaskModel = {title: this.taskTitle, important: this.important};
+
+  onFocusIn(): void {
+    this.showSubmitButton = true;
+    this.showTip = true;
+  }
+
+  onFocusOut() {
+    this.showTip = false;
+  }
+
+  saveTask() {
+    this.task = {
+      title: this.taskTitle,
+      important: this.important
+    }
+    this.taskService.create(this.task);
+    console.log(this.task);
+    this.taskTitle = '';
+  }
+}
